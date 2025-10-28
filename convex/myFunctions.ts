d07293e29ec678357b854322caa90d9596c4ea27
Lyn <0xlynett@proton.me>
2025-10-28 16:27:06 +0000

    fix: typescript errors

--- convex/myFunctions.ts
+++ convex/myFunctions.ts
@@ -1,19 +1,18 @@
-import { v } from 'convex/values'
-import { action, mutation, query } from './_generated/server'
-import { api, components } from './_generated/api'
+import { mutation, query } from './_generated/server'
+import { components } from './_generated/api'
 import { ShardedCounter } from '@convex-dev/sharded-counter'
 
 const counter = new ShardedCounter(components.shardedCounter)
 
 export const milady = mutation({
   args: {},
-  handler: async (ctx, args) => {
+  handler: async (ctx) => {
     await counter.inc(ctx, 'milady')
   },
 })
 export const getCount = query({
   args: {},
-  handler: async (ctx, _args) => {
+  handler: async (ctx) => {
     return await counter.count(ctx, 'milady')
   },
 })
